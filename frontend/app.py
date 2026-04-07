8ad7e50cfa2a0be8ffcb99f8a652be66c0bd58d1
Biprajit541 <biprajitdas2020@gmail.com>
2026-04-08 03:05:19 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://ai-study-companion-production-33d6.up.railway.app"
+API_URL = "http://helpful-balance-production-f861.up.railway.app"
 
 st.set_page_config(page_title="AI Study Companion", layout="wide")
 
