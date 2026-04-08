49f1ea5b7f8e92ad0402d6ccf3adb7ae3a6a597d
Biprajit541 <biprajitdas2020@gmail.com>
2026-04-08 11:14:22 +0530

    Update llm.py

--- backend/services/llm.py
+++ backend/services/llm.py
@@ -3,14 +3,11 @@ import os
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-def generate_response(prompt: str):
+def generate_response(messages):
     try:
         response = client.chat.completions.create(
             model="llama-3.1-8b-instant",
-            messages=[
-                {"role": "system", "content": "You are a helpful AI tutor."},
-                {"role": "user", "content": prompt}
-            ]
+            messages=messages
         )
 
         return response.choices[0].message.content
